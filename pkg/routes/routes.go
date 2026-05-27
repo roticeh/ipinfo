8bc673a12e8c96c16f10318adb47222010dccf51
onurartan <artanomer13@gmail.com>
2026-05-27 10:45:19 +0300

    chore: add default config template to fix docker mount issue

--- pkg/routes/routes.go
+++ pkg/routes/routes.go
@@ -1,26 +1,32 @@
 package routes
 
 import (
+	"time"
+
 	"github.com/gofiber/fiber/v2"
+
+	config "github.com/roticeh/ipinfo/pkg/config"
 	handlers "github.com/roticeh/ipinfo/pkg/handlers"
 	middleware "github.com/roticeh/ipinfo/pkg/middleware"
 )
 
 func SetupRoutes(app fiber.Router) {
-	ipGroup := app.Group("/ip", middleware.CorsGuard(), middleware.GeneralLimiter())
+	ipGroup := app.Group("/ip")
+	ipGroupWithGuard := ipGroup.Group("", middleware.CorsGuard(), middleware.GeneralLimiter())
 
 	// GET /v1/ip/me -> Analyzes the calling client's own IP and User-Agent data.
-	ipGroup.Get("/me", handlers.GetMyIPInfo)
-
+	ipGroup.Get("/me", handlers.GetMyIPInfo, middleware.PublicCors(), middleware.GeneralLimiter(config.RateLimitConfig{
+		Max:        25,
+		Expiration: 1 * time.Minute,
+	}))
 
 	// POST /v1/ip/bulk -> Resolves multiple IP addresses simultaneously within a single payload.
-	ipGroup.Post("/bulk", handlers.GetBulkIPInfo)
+	ipGroupWithGuard.Post("/bulk", handlers.GetBulkIPInfo)
 
 	// GET /v1/ip/:ipaddress -> Analyzes a specific target IP address passed as a parameter.
-	ipGroup.Get("/:ipaddress", handlers.GetSpecificIPInfo)
+	ipGroupWithGuard.Get("/:ipaddress", handlers.GetSpecificIPInfo)
 
 	// GET /v1/ip/:ipaddress/:field -> Extracts a specific data block (e.g., /location, /network, /device) dynamically.
-	ipGroup.Get("/:ipaddress/:field", handlers.GetSpecificFieldInfo)
+	ipGroupWithGuard.Get("/:ipaddress/:field", handlers.GetSpecificFieldInfo)
 
-	
 }
